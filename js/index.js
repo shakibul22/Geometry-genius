e088d672a5cc0c66e106d8fefc8d87c787a18ec1
Shakibul Islam <shakibulislam684@gmail.com>
2023-02-18 20:44:48 +0600

    question-answer

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+let serial =0;
 document.getElementById('blog-button').addEventListener('click', ()=>{
     document.getElementById('home-page').style.display= 'none';
     document.getElementById('blogs').style.display= 'block';
@@ -9,11 +10,75 @@ document.getElementById('header-icon').addEventListener('click', ()=>{
 
 })
 
-document.getElementById('triangle-btn').addEventListener('click', () => {
-    const sd = getData('triangle-name','triangle-base','triangle-height')
-    const area= 0.5*sd.firstInputValue*sd.secondInputValue;
+document.getElementById('ellipse-btn').addEventListener('click', () => {
+    serial+=1;
+    const sd = getData('ellipse-name','ellipsee-base','ellipse-height')
+    const area= 3.1416*sd.firstInputValue*sd.secondInputValue;
+    displayData(sd.shapeName,area)
     
 })
+document.getElementById('pentagon-btn').addEventListener('click', () => {
+    serial+=1;
+    const sd = getData('pentagon-name','pentagon-base','pentagon-height')
+    const area= 0.5*sd.firstInputValue*sd.secondInputValue;
+    displayData(sd.shapeName,area)
+    
+})
+document.getElementById('rhombus-btn').addEventListener('click', () => {
+    serial+=1;
+    const sd = getData('rhombus-name','rhombus-base','rhombus-height')
+    const area= 0.5*sd.firstInputValue*sd.secondInputValue;
+    displayData(sd.shapeName,area)
+    
+})
+document.getElementById('parallelogram-btn').addEventListener('click', () => {
+    serial+=1;
+    const sd = getData('parallelogram-name','parallelogram-base','parallelogram-height')
+    const area= 0.5*sd.firstInputValue*sd.secondInputValue;
+    displayData(sd.shapeName,area)
+    
+})
+document.getElementById('rectangle-btn').addEventListener('click', () => {
+    serial+=1;
+    const sd = getData('rectangle-name','rectangle-width','rectangle-length')
+    
+
+    if (
+        rectangle-width == "" ||
+        rectangle-length == "" ||
+        rectangle-width <= 0 ||
+        rectangle-length <= 0
+      ) {
+        return alert("please enter any valid number");
+      }
+      const area= purseFloat( sd.firstInputValue)*parseFloat(sd.secondInputValue);
+      displayData(sd.shapeName,area)
+      
+    
+})
+document.getElementById('triangle-btn').addEventListener('click', () => {
+    serial+=1;
+    const sd = getData('triangle-name','triangle-base','triangle-height')
+    const area= 0.5*sd.firstInputValue*sd.secondInputValue;
+    displayData(sd.shapeName,area)
+    
+})
+
+function displayData(nameOfShape, area) {
+    const container = document.getElementById("area");
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td>${serial}</td>
+      <td>${nameOfShape}</td>
+      <td>${area} cm <sup>2</sup></sup></td>
+      
+      <td>
+      <button class="btn btn-sm btn-red-500 ">X</button>
+      </td>
+      
+    `;
+    container.appendChild(tr);
+  }
 
 function getData(id1, id2, id3) {
 
