4b121cdc9593271028e91a2b17a671e91ed9a957
Shakibul Islam <shakibulislam684@gmail.com>
2023-02-18 23:43:51 +0600

    update-validation

--- js/index.js
+++ js/index.js
@@ -1,69 +1,62 @@
-let serial =0;
-document.getElementById('blog-button').addEventListener('click', ()=>{
-    document.getElementById('home-page').style.display= 'none';
-    document.getElementById('blogs').style.display= 'block';
+let serial = 0;
+document.getElementById('blog-button').addEventListener('click', () => {
+    document.getElementById('home-page').style.display = 'none';
+    document.getElementById('blogs').style.display = 'block';
 
 })
-document.getElementById('header-icon').addEventListener('click', ()=>{
-    document.getElementById('home-page').style.display= 'flex';
-    document.getElementById('blogs').style.display= 'none';
+document.getElementById('header-icon').addEventListener('click', () => {
+    document.getElementById('home-page').style.display = 'flex';
+    document.getElementById('blogs').style.display = 'none';
 
 })
 
 
 document.getElementById('ellipse-btn').addEventListener('click', () => {
-    serial+=1;
-    const sd = getData('ellipse-name','ellipsee-base','ellipse-height')
-    const area= 3.1416*sd.firstInputValue*sd.secondInputValue;
-    displayData(sd.shapeName,area)
-    
+    serial += 1;
+    const sd = getData('ellipse-name', 'ellipsee-base', 'ellipse-height')
+    const area = 3.1416 * sd.firstInputValue * sd.secondInputValue;
+    displayData(sd.shapeName, area)
+
 })
 document.getElementById('pentagon-btn').addEventListener('click', () => {
-    serial+=1;
-    const sd = getData('pentagon-name','pentagon-base','pentagon-height')
-    const area= 0.5*sd.firstInputValue*sd.secondInputValue;
-    displayData(sd.shapeName,area)
-    
+    serial += 1;
+    const sd = getData('pentagon-name', 'pentagon-base', 'pentagon-height')
+    const area = 0.5 * sd.firstInputValue * sd.secondInputValue;
+    displayData(sd.shapeName, area)
+
 })
 document.getElementById('rhombus-btn').addEventListener('click', () => {
-    serial+=1;
-    const sd = getData('rhombus-name','rhombus-base','rhombus-height')
-    const area= 0.5*sd.firstInputValue*sd.secondInputValue;
-    displayData(sd.shapeName,area)
-    
+    serial += 1;
+    const sd = getData('rhombus-name', 'rhombus-base', 'rhombus-height')
+    const area = 0.5 * sd.firstInputValue * sd.secondInputValue;
+    displayData(sd.shapeName, area)
+
 })
 document.getElementById('parallelogram-btn').addEventListener('click', () => {
-    serial+=1;
-    const sd = getData('parallelogram-name','parallelogram-base','parallelogram-height')
-    const area= 0.5*sd.firstInputValue*sd.secondInputValue;
-    displayData(sd.shapeName,area)
-    
+    serial += 1;
+    const sd = getData('parallelogram-name', 'parallelogram-base', 'parallelogram-height')
+    const area = 0.5 * sd.firstInputValue * sd.secondInputValue;
+    displayData(sd.shapeName, area)
+
 })
 document.getElementById('rectangle-btn').addEventListener('click', () => {
-    serial+=1;
-    const sd = getData('rectangle-name','rectangle-width','rectangle-length')
-    
+    serial += 1;
+    const sd = getData('rectangle-name', 'rectangle-width', 'rectangle-length')
 
-    if (
-        rectangle-width == "" ||
-        rectangle-length == "" ||
-        rectangle-width <= 0 ||
-        rectangle-length <= 0
-      ) {
-        return alert("please enter any valid number");
-      }
-      const area= purseFloat( sd.firstInputValue)*parseFloat(sd.secondInputValue);
-      displayData(sd.shapeName,area)
-      
-    
+
+  
+    const area = parseFloat(sd.firstInputValue) * parseFloat(sd.secondInputValue);
+    displayData(sd.shapeName, area)
+
+
 })
 document.getElementById('triangle-btn').addEventListener('click', () => {
-    
-    serial+=1;
-    const sd = getData('triangle-name','triangle-base','triangle-height')
-    const area= 0.5*sd.firstInputValue*sd.secondInputValue;
-    displayData(sd.shapeName,area)
-    
+
+    serial += 1;
+    const sd = getData('triangle-name', 'triangle-base', 'triangle-height')
+    const area = 0.5 * sd.firstInputValue * sd.secondInputValue;
+    displayData(sd.shapeName, area)
+
 })
 
 function displayData(nameOfShape, area) {
@@ -80,19 +73,22 @@ function displayData(nameOfShape, area) {
       
     `;
     container.appendChild(tr);
-  }
+}
 
 function getData(id1, id2, id3) {
 
     const shapeName = document.getElementById(id1).innerText;
     const firstInputValue = document.getElementById(id2).value;
     const secondInputValue = document.getElementById(id3).value;
-  
+    if (firstInputValue == "" || firstInputValue <= 0 || secondInputValue == "" || secondInputValue <= 0) {
+        return alert("please enter any valid number");
+    }
     const sd = {
-      shapeName: shapeName,
-      firstInputValue: firstInputValue,
-      secondInputValue: secondInputValue,
+        shapeName: shapeName,
+        firstInputValue: firstInputValue,
+        secondInputValue: secondInputValue,
     };
     return sd;
-  }
-  
+}
+
+
